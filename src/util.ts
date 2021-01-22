1d45f4f07a67c8223f36b9b982fb6a485f0ee78e
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2021-01-22 19:43:53 +0200

    Add sql.values for constructing VALUES lists

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,9 @@
+export const isArray = Array.isArray
+
+export function isObject(value: unknown): value is Record<string, unknown> {
+  return typeof value === 'object' && value !== null
+}
+
 export function isString(value: unknown): value is string {
   return typeof value === 'string'
 }
